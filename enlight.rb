4b8b892e85dd4c94402e2194180c6d5818747ee4
Dan Pickett <dpickett@enlightsolutions.com>
2009-05-07 14:50:03 +0800

    bumping the version of will_paginate
    Signed-off-by: Thomas R. Koll <tomk32@tomk32.de>

--- enlight.rb
+++ enlight.rb
@@ -188,7 +188,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', 
              :lib => 'will_paginate', 
              :source => 'http://gems.github.com', 
-             :version => '~> 2.3.5'
+             :version => '>= 2.3.8'
   config.gem 'rr', 
              :version => '>= 0.9.0'
   config.gem 'thoughtbot-factory_girl', 
